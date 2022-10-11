5ceb172828318291aba58fcda132b20dbdd270c7
Jorge Ramírez <jorge.ramirez@jungheinrich.es>
2022-10-11 09:24:11 +0200

    Update Project template files
    Updates the whole project to use the current template system created by VS for MAUI projects.

--- src/Platforms/iOS/AppDelegate.cs
+++ src/Platforms/iOS/AppDelegate.cs
@@ -1,12 +1,10 @@
 ﻿using Foundation;
-using Microsoft.Maui;
-using Microsoft.Maui.Hosting;
 
 namespace HelloFreshMauiPreview
 {
-	[Register("AppDelegate")]
-	public class AppDelegate : MauiUIApplicationDelegate
-	{
-		protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-	}
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
 }
